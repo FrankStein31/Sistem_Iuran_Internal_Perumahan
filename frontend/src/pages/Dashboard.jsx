1317b66c7c959177f0670d96ee5e3aed7b205949
Frankie Steinlie <frankie.intern24slides@gmail.com>
2026-04-10 16:22:34 +0700

    done all 100%

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -74,7 +74,7 @@ const Dashboard = () => {
                         </div>
                         <div className="ml-5 w-0 flex-1">
                             <dl>
-                                <dt className="text-sm font-medium text-gray-500 truncate">Total Penghuni</dt>
+                                <dt className="text-sm font-medium text-gray-500 truncate">Total Data Penghuni</dt>
                                 <dd className="flex items-baseline">
                                     <div className="text-2xl font-semibold text-gray-900">{summary.total_penghuni}</div>
                                 </dd>
